17dd535afdcee60291f575b00198d75206187f49
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2022-05-08 20:51:32 +0530

    Add files via upload
    This program is about how to insert values on run-time in a Binary Tree.

--- Tree.java
+++ Tree.java
@@ -1,21 +1,50 @@
 import java.util.*;
 
 class Node{
-	int item;
-	Node left,right;
-
-	Node(int key){
-
-		item = key;
-		right = left = null;
-	}
+	int key;
+	Node left = null;
+    	Node right = null;
 }
 
 class BinaryTree{
 	Node root;
+	static Queue<Node> Q = new LinkedList<>();
 
 	BinaryTree(){
 		root = null;
+	}
+	
+	Node newNode(int value){
+        	Node n = new Node();
+        	n.key = value;
+        	n.left = null;
+        	n.right = null;
+        	return n;
+	}
+	
+	int insert(int value){
+        	
+                Node node = newNode(value);
+                if(root == null)
+                    root = node;
+                     
+                else if(Q.peek().left == null)
+                    Q.peek().left = node;  
+                
+                else{
+                    Q.peek().right = node;
+                    Q.remove();
+                }
+                
+                Q.add(node);
+
+		return 0;
+	}
+	
+	void create(int arr[],int n){
+        	for(int i=0;i<n;i++){
+                	insert(arr[i]);   
+        	}   
 	}
 
 	void postorder(Node node){
@@ -26,7 +55,7 @@ class BinaryTree{
 
 		postorder(node.right);
 
-		System.out.print(node.item + "->");
+		System.out.print(node.key + "->");
 	}	
 
 	void inorder(Node node){
@@ -34,7 +63,7 @@ class BinaryTree{
 			return;
 
 		inorder(node.left);
-		System.out.print(node.item + "->");
+		System.out.print(node.key + "->");
 		inorder(node.right);
 	}
 
@@ -42,7 +71,7 @@ class BinaryTree{
 		if(node == null)
 			return;
 
-		System.out.print(node.item + "->");
+		System.out.print(node.key + "->");
 		preorder(node.left);
 		preorder(node.right);
 	}
@@ -50,15 +79,28 @@ class BinaryTree{
 
 public class Tree{
 	public static void main(String arg[]){
+		Scanner sc = new Scanner(System.in);
 		BinaryTree tree = new BinaryTree();
 
-		tree.root = new Node(1);
+		System.out.print("Enter the number of elements in Tree: ");
+		int n = sc.nextInt();
+
+		int value;
+		int arr[] = new int[n];
+
+		System.out.print("Enter values to insert in the tree: ");
+		for(int i=0;i<n;i++){
+			arr[i] = sc.nextInt();
+		}
+
+        	tree.create(arr,n);
+		/*tree.root = new Node(1);
 		tree.root.left = new Node(2);
 		tree.root.right = new Node(3);
 		tree.root.left.left = new Node(4);
 		tree.root.left.right = new Node(5);
 		tree.root.right.left = new Node(6);
-		tree.root.right.right = new Node(7);
+		tree.root.right.right = new Node(7);*/
 
 		System.out.print("Postorder: ");
 		tree.postorder(tree.root);
